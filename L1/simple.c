4f1d43a5961c786e57082eb59e280354f85b83eb
robertvardy <robertjvardy@gmail.com>
2021-01-23 18:51:53 -0500

    changes

--- L1/simple.c
+++ L1/simple.c
@@ -1,39 +1,26 @@
 #include <linux/init.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/jiffies.h>
+#include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
 
 #define BUFFER_SIZE 128
 #define PROC_NAME "hello"
 
-ssize_t proc_read(struct file *file, char __user *usr_buf,
-       size_t count, loff_t *pos);
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
 
 static struct file_operations proc_ops = {
-       .owner = THIS_MODULE,
-       .read = proc_read,
+.owner = THIS_MODULE,
+.read = proc_read,
 };
 
-int simple_init(void)
+int proc_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
        proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-
        return 0;
 }
 
-
-void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
-	remove_proc_entry(PROC_NAME, NULL);
+void proc_exit(void)
+{
+       remove_proc_entry(PROC_NAME, NULL);
 }
-
-module_init( simple_init );
-module_exit( simple_exit );
-
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Simple Module");
-MODULE_AUTHOR("SGG");
-
