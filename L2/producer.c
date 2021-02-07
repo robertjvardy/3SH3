9dd7f0e83e1143b14047f54dbcf58e6f72eb8814
robertjvardy <robertjvardy@gmail.com>
2021-02-07 13:50:31 -0700

    fixed books code

--- L2/producer.c
+++ L2/producer.c
@@ -5,30 +5,27 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <unistd.h>
 int main()
 {
-    /* the size (in bytes) of shared memory object */
     const int SIZE = 4096;
-    /* name of the shared memory object */
+    
     const char *name = "OS";
-    /* strings written to shared memory */
-    const char *message 0 = "Hello";
-    const char *message 1 = "World!";
-    /* shared memory file descriptor */
+    
+    const char *message_0 = "Test\n";
+
     int fd;
-    /* pointer to shared memory obect */
+    
     char *ptr;
-    /* create the shared memory object */
+    
     fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-    /* configure the size of the shared memory object */
+
     ftruncate(fd, SIZE);
-    /* memory map the shared memory object */
-    ptr = (char *)
-        mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    /* write to the shared memory object */
+    
+    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
     sprintf(ptr, "%s", message_0);
     ptr += strlen(message_0);
-    sprintf(ptr, "%s", message_1);
-    ptr += strlen(message_1);
+
     return 0;
 }
