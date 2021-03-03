5f1d90228843527cd9f43a8573d9c4bee005b187
robertvardy <robertjvardy@gmail.com>
2021-03-02 21:29:04 -0500

    fixed primes

--- L3/threaded-primes.c
+++ L3/threaded-primes.c
@@ -3,57 +3,33 @@
 #include <pthread.h>
 #include <math.h>
 
-int prime;
+int value;
 
-void *thread_function_print_primes();
+void *primes_thread();
 int is_prime(int num);
 
 int main(int argc, char *argv[])
 {
 
-    printf("Prime Number Calculator\n");
+    value = atoi(argv[1]);
+    pthread_t primes_id;
 
-    prime = atoi(argv[1]);
-    printf("all primes up to: %d\n", prime);
-
-    pthread_t thread_primes;
-
-    pthread_create(&thread_primes, NULL, thread_function_print_primes, NULL);
-
-    pthread_join(thread_primes, NULL);
-
-    return 0;
-}
-
-void *thread_function_print_primes()
-{
-
-    pthread_t id = pthread_self();
-
-    printf("The prime numbers are:\n");
-    for (int i = 2; i <= prime; i++)
-    {
-        if (is_prime(i))
-        {
-            printf("%d, ", i);
-        }
-    }
-
-    printf("\nThread #%lu finished\n", id);
+    pthread_create(&primes_id, NULL, primes_thread, NULL);
+    pthread_join(primes_id, NULL);
 
     return 0;
 }
 
 int is_prime(int num)
 {
+    int max = sqrt(num) + 1;
     if (num % 2 == 0)
     {
         return 0;
     }
     else
     {
-        int sr = sqrt(num) + 1;
-        for (int i = 3; i <= sr; i++)
+        for (int i = 3; i <= max; i = i + 1)
         {
             if (num % i == 0)
             {
@@ -63,4 +39,25 @@ int is_prime(int num)
     }
 
     return 1;
+}
+
+void *primes_thread()
+{
+    printf("Prime numbers less than %d:\n", value);
+    if (value == 1)
+    {
+        return 0
+    }
+    else
+    {
+
+        for (int i = 2; i <= value; i++)
+        {
+            if (is_prime(i))
+            {
+                printf("%d, ", i);
+            }
+        }
+    }
+    return 0;
 }
