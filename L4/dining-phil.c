d89aad41937e7e6a194240b392187d42246f363b
robertvardy <robertjvardy@gmail.com>
2021-03-11 21:07:08 -0500

    remove name

--- L4/dining-phil.c
+++ L4/dining-phil.c
@@ -5,7 +5,7 @@
 /* The threads will serve as the philosphers */
 #define THREAD_NUM 5
 
-void *thread_function(void *parameter, int phil_num);
+void *thread_function(int phil_num);
 
 int counter = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -13,15 +13,10 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int main()
 {
     pthread_t threadId[THREAD_NUM];
-    char *threadName[THREAD_NUM] = {"Thread 1",
-                                    "Thread 2",
-                                    "Thread 3",
-                                    "Thread 4",
-                                    "Thread 5"};
     int ret;
     for (int i = 0; i < THREAD_NUM; i++)
     {
-        ret = pthread_create(&threadId[i], NULL, thread_function, threadName[i], i);
+        ret = pthread_create(&threadId[i], NULL, thread_function, i);
         if (ret != 0)
         {
             fprintf(stderr, "Create thread failed!\n");
@@ -39,10 +34,9 @@ int main()
     return 0;
 }
 
-void *thread_function(void *parameter, int phil_num)
+void *thread_function(int phil_num)
 {
-    const char *name = (char *)parameter;
-    printf("%s %d start!\n", name, phil_num);
+    printf("%d start!\n", phil_num);
     pthread_mutex_lock(&mutex);
     for (int i = 0; i < 10000; i++)
     {
@@ -54,5 +48,5 @@ void *thread_function(void *parameter, int phil_num)
     printf("Sleeping for %d seconds\n", randomNumber);
     sleep(randomNumber);
 
-    printf("%s complete!\n", name);
+    printf("%d complete!\n", phil_num);
 }
